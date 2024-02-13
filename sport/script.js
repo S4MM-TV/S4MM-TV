58652752f74bd5535a6ea27624cd901e3631dd47
S4MM-TV <138916386+S4MM-TV@users.noreply.github.com>
2024-02-13 20:02:57 +0300

    Update script.js

--- sport/script.js
+++ sport/script.js
@@ -1,5 +1,5 @@
 
-const url = "http://s4mm-tv.github.io/sport/sppppm3.html"
+const url = "https://s4mm-tv.github.io/sport/sppppm3.html"
 fetch(url)
   .then((response) => {
     const urlData = response.json();
