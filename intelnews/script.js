c4f5f9c2e06264aa05052dfd27d61b4fb04b5e75
S4MM-TV <138916386+S4MM-TV@users.noreply.github.com>
2024-02-13 22:36:07 +0300

    Update script.js

--- intelnews/script.js
+++ intelnews/script.js
@@ -1,4 +1,4 @@
-const url = "https://d50c5e3b-c5e0-4242-9b50-a42bfba8130b-00-2fnuk8nrxnizu.sisko.replit.dev/IntNews/newssm3.html"
+const url = "https://s4mm-tv.github.io/intelnews/newssm3.html"
 fetch(url)
   .then((response) => {
   const urlData = response.json();
